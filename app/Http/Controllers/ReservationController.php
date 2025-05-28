e83c7d437c5f6a98dd733abe725092bbd872dce2
shaherHA <shaherahmad207@gmail.com>
2025-05-28 15:40:33 +0200

    Worked on reservation page dynamic generation

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,11 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Room;
+use Carbon\Carbon;
+use Carbon\CarbonImmutable;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    public function index() {
-        return view('reservations.index');
+    public function index(Request $request) {
+
+        $date = CarbonImmutable::now();
+
+        if ($request->input('date')) {
+            $date = CarbonImmutable::parse($request->input('date'));
+        }
+
+        $dates = [];
+
+        for ($i = 0; $i < 14; $i++) {
+            $dates[] = CarbonImmutable::parse($date->addDays($i)->toDateString());
+        }
+
+        $rooms = Room::with('reservations')->get();
+
+        // for each room display the reservations in a date range
+        // date range is two weeks from current date OR user submitted date
+
+        return view('reservations.index', compact('dates', 'rooms'));
     }
 }
--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -1,3 +1,4 @@
+@php use Carbon\Carbon; @endphp
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-full mx-auto sm:px-6 lg:px-8">
@@ -9,27 +10,48 @@
                         <table class="min-w-full bg-white">
                             <thead>
                             <tr class="bg-gray-100 text-gray-700 text-xs">
-                                <th class="py-2 px-4 border sticky left-0 bg-gray-100 z-10">Room</th>
-                                <th class="py-2 px-4 border">Mon</th>
-                                <th class="py-2 px-4 border">Tue</th>
-                                <th class="py-2 px-4 border">Wed</th>
-                                <th class="py-2 px-4 border">Thu</th>
-                                <th class="py-2 px-4 border">Fri</th>
-                                <th class="py-2 px-4 border">Sat</th>
-                                <th class="py-2 px-4 border">Sun</th>
-                                <th class="py-2 px-4 border">Mon</th>
-                                <th class="py-2 px-4 border">Tue</th>
-                                <th class="py-2 px-4 border">Wed</th>
-                                <th class="py-2 px-4 border">Thu</th>
-                                <th class="py-2 px-4 border">Fri</th>
-                                <th class="py-2 px-4 border">Sat</th>
-                                <th class="py-2 px-4 border">Sun</th>
+                                <th class="py-2 px-4 border sticky bg-gray-100 z-10">Room</th>
+                                @foreach($dates as $date)
+                                    <x-reservation-head>{{ $date->shortDayName }} {{ $date->format('m-d') }}</x-reservation-head>
+                                @endforeach
                             </tr>
                             </thead>
                             <tbody>
-                            <!-- Room 101 -->
-                            <tr>
-                                <td class="py-2 px-4 border font-medium sticky left-0 bg-white z-10">101</td>
+                            @foreach($rooms as $room)
+                                <tr>
+                                    <td class="py-2 px-4 border font-medium sticky left-0 bg-white z-10">{{ $room->room_number }}</td>
+
+                                    @foreach($dates as $date)
+                                        @php
+                                            $hasReservation = false;
+                                            $colspan = 1; // Default colspan
+                                        @endphp
+
+                                        @foreach($room->reservations as $reservation)
+                                            @php
+                                                if ($reservation->start_date === $date->toDateString()) {
+                                                    echo $reservation->start_date;
+                                                    echo $date->toDateString();
+                                                $start_date = Carbon::parse($reservation->start_date);
+                                                $end_date = Carbon::parse($reservation->end_date);
+                                                $colspan = intval($start_date->diffInDays($end_date) + 1);
+                                                $hasReservation = true;
+                                                }
+                                            @endphp
+                                        @endforeach
+
+                                        @if ($hasReservation)
+                                            <td class="py-2 px-4 border bg-blue-100 rounded-full" colspan="{{ $colspan }}">
+                                                <div class="text-xs">John Doe (9:00-12:00)</div>
+                                            </td>
+                                        @elseif (!$hasReservation || $colspan === 1)
+                                            <td class="py-2 px-4 border"></td>
+                                        @endif
+                                    @endforeach
+                                </tr>
+                            @endforeach
+
+                            <!--
                                 <td class="py-2 px-4 border"></td>
                                 <td class="py-2 px-4 border bg-blue-100 rounded-full" colspan="5">
                                     <div class="text-xs">John Doe (9:00-12:00)</div>
@@ -42,70 +64,7 @@
                                 <td class="py-2 px-4 border"></td>
                                 <td class="py-2 px-4 border"></td>
                                 <td class="py-2 px-4 border"></td>
-                            </tr>
-
-                            <!-- Room 102 -->
-                            <tr>
-                                <td class="py-2 px-4 border font-medium sticky left-0 bg-white z-10">102</td>
-                                <td class="py-2 px-4 border bg-purple-100" colspan="5">
-                                    <div class="text-xs">Mary Johnson (8:00-13:00)</div>
-                                </td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border bg-yellow-100" colspan="3">
-                                    <div class="text-xs">Robert Brown (16:00-19:00)</div>
-                                </td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border"></td>
-                            </tr>
-
-                            <!-- Room 103 -->
-                            <tr>
-                                <td class="py-2 px-4 border font-medium sticky left-0 bg-white z-10">103</td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border bg-red-100" colspan="6">
-                                    <div class="text-xs">David Wilson (12:00-18:00)</div>
-                                </td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border bg-indigo-100" colspan="3">
-                                    <div class="text-xs">Sarah Lee (20:00-23:00)</div>
-                                </td>
-                            </tr>
-
-                            <!-- Room 104 -->
-                            <tr>
-                                <td class="py-2 px-4 border font-medium sticky left-0 bg-white z-10">104</td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border bg-orange-100" colspan="4">
-                                    <div class="text-xs">James Taylor (10:00-14:00)</div>
-                                </td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border bg-teal-100" colspan="5">
-                                    <div class="text-xs">Jennifer Clark (17:00-22:00)</div>
-                                </td>
-                            </tr>
-
-                            <!-- Room 105 -->
-                            <tr>
-                                <td class="py-2 px-4 border font-medium sticky left-0 bg-white z-10">105</td>
-                                <td class="py-2 px-4 border bg-pink-100" colspan="8">
-                                    <div class="text-xs">Michael Adams (8:00-16:00)</div>
-                                </td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border"></td>
-                                <td class="py-2 px-4 border"></td>
-                            </tr>
+                            </tr> -->
                             </tbody>
                         </table>
                     </div>
