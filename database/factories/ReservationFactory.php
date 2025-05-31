8e0c07557ea1bb3baaae9d7d7f953cbdfdbb413d
shaherHA <shaherahmad207@gmail.com>
2025-05-31 18:21:35 +0200

    feature:implemented reservation page

--- database/factories/ReservationFactory.php
+++ database/factories/ReservationFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Enums\RolesEnum;
+use App\Models\Room;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +19,25 @@ class ReservationFactory extends Factory
      */
     public function definition(): array
     {
+
+        $minRoom = Room::min('id');
+        $maxRoom = Room::max('id');
+
+        $minUser = User::where(
+            'role', '=', RolesEnum::CUSTOMER
+        )->min('id');
+        $maxUser = User::where(
+            'role', '=', RolesEnum::CUSTOMER
+        )->max('id');
+
+        $startDate = $this->faker->dateTimeBetween('2025-05-30', '2025-06-06');
+        $duration = $this->faker->numberBetween(2, 5);
+        $endDate = (clone $startDate)->modify("+{$duration} days");
         return [
-            //
+            'room_id' => $this->faker->numberBetween($minRoom, $maxRoom),
+            'user_id' => $this->faker->numberBetween($minUser, $maxUser),
+            'start_date' => $startDate,
+            'end_date' => $endDate,
         ];
     }
 }
